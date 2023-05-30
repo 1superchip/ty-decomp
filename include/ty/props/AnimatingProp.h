e5dee12a1ae5aac57162c5b2cfdbb3c34f56f40c
1superchip <1superchip@gmail.com>
2023-05-30 08:36:24 -0500

    Match and link AnimatingProp.cpp

---
+++ include/ty/props/AnimatingProp.h
@@ -0,0 +1,102 @@
+#ifndef ANIMATINGPROP_H
+#define ANIMATINGPROP_H
+
+#include "ty/props/StaticProp.h"
+#include "ty/CommonGameObjectFlags.h"
+#include "common/MKAnimScript.h"
+#include "ty/tools.h"
+
+// should not go here
+struct SoundEventHelper {
+	int unk0;
+	
+	void Update(int, bool, bool, GameObject*, Vector*, float, int);
+    void Init(void) {
+        unk0 = -1;
+    }
+};
+struct SoundEvent {
+    int soundEventIndex;
+    char* animEventName;
+};
+
+void AnimatingProp_LoadResources(KromeIni* pIni);
+
+struct AnimatingPropDesc : StaticPropDescriptor {
+    MKAnimScript mAnimScript;
+    Tools_AnimDesc mAnimDesc;
+    Tools_AnimEventDesc mAnimEventDesc;
+    bool unk10C;
+    bool unk10D;
+    u16 defaultGameObjFlags;
+    int soundEventIndex;
+    int nmbrOfSoundEvents;
+    SoundEvent* pSoundEvents;
+
+    void Init(ModuleInfoBase *pModuleBase, char *param_2, char *param_3, int param_4,
+            int param_5, Tools_AnimEntry *pAnimEntry, int animEntryCount, Tools_AnimEvent *pAnimEvent, int animEventCount,
+            bool param_10, u16 _defaultGameObjFlags);
+    virtual void Load(KromeIni* pIni);
+};
+
+struct AnimatingProp : StaticProp {
+    MKAnimScript mAnimScript;
+    Tools_AnimManager mAnimManager;
+    Tools_AnimEventManager mAnimEventManager;
+    SoundEventHelper soundHelper;
+    CommonGameObjFlagsComponent gameObjFlags;
+
+    virtual void Init(GameObjDesc*);
+    virtual bool LoadLine(KromeIniLine*);
+    virtual void LoadDone(void);
+    virtual void Reset(void);
+    virtual void Update(void);
+    virtual void Draw(void);
+    virtual void Message(MKMessage*);
+    virtual void Show(bool bShow);
+    virtual void Activate(bool bActivate);
+    virtual void Enable(bool bEnable);
+
+    AnimatingPropDesc* GetDesc(void) {
+        return descr_cast<AnimatingPropDesc*>(pDescriptor);
+    }
+};
+
+extern "C" void memcpy(void*, void*, int);
+
+template <typename T>
+void LoadAnimPropDescriptors(KromeIni* pIni, char* name, T* pDesc) {
+    GameObjDesc* desc = NULL;
+    KromeIniLine* pLine = pIni->GotoLine(name, NULL);
+    while (pLine != NULL && (pLine->section != NULL || pLine->pFieldName != NULL || pLine->comment != NULL)) {
+        char* pExactName = NULL;
+        if (pLine->pFieldName != NULL && pLine->AsString(0, &pExactName) != false) {
+            T* pCurrDesc = (T*)Heap_MemAlloc(sizeof(T));
+            memset(pCurrDesc, 0, sizeof(T));
+            new ((void*)pCurrDesc) T;
+            Tools_AnimEntry* pAnimEntries = NULL;
+            if (pDesc->mAnimDesc.unk4) {
+                pAnimEntries = (Tools_AnimEntry*)Heap_MemAlloc(pDesc->mAnimDesc.unk4 * sizeof(Tools_AnimEntry));
+                memcpy(pAnimEntries, (void*)pDesc->mAnimDesc.pEntries, pDesc->mAnimDesc.unk4 * sizeof(Tools_AnimEntry));
+            }
+            Tools_AnimEvent* pAnimEventEntries = NULL;
+            if (pDesc->mAnimEventDesc.mCount) {
+                pAnimEventEntries = (Tools_AnimEvent*)Heap_MemAlloc(pDesc->mAnimEventDesc.mCount * sizeof(Tools_AnimEvent));
+                memcpy(pAnimEventEntries, (void*)pDesc->mAnimEventDesc.pEvents, pDesc->mAnimEventDesc.mCount * sizeof(Tools_AnimEvent));
+            }
+            pCurrDesc->Init(pDesc->pModule, pExactName, pLine->pFieldName, pDesc->searchMask, pDesc->flags,
+                pAnimEntries, pDesc->mAnimDesc.unk4, pAnimEventEntries, pDesc->mAnimEventDesc.mCount, pDesc->unk10C, pDesc->defaultGameObjFlags);
+            pCurrDesc->unk80 = desc;
+            desc = (GameObjDesc*)pCurrDesc;
+        }
+        pLine = pIni->GetLineWithLine(pLine);
+    }
+    while (desc != NULL) {
+        GameObjDesc* p = desc;
+        desc = p->unk80;
+        p->Load(pIni);
+        objectManager.AddDescriptor(p);
+    }
+}
+
+#endif // ANIMATINGPROP_H
---
+++ src/ty/source/props/AnimatingProp.cpp
@@ -0,0 +1,184 @@
+#include "ty/props/AnimatingProp.h"
+#include "ty/GameObjectManager.h"
+#include "common/Str.h"
+#include "common/system_extras.h"
+int SoundBank_ResolveSoundEventIndex(char*);
+
+static ModuleInfo<AnimatingProp> animPropModInfo;
+
+void AnimatingProp_LoadResources(KromeIni* pIni) {
+    AnimatingPropDesc animPropDescDefault;
+    animPropDescDefault.Init(&animPropModInfo, "", "", 1, 0, (Tools_AnimEntry*)NULL,
+            0, (Tools_AnimEvent*)NULL, 0, 1, GameObjFlags_All);
+    LoadAnimPropDescriptors<AnimatingPropDesc>(pIni, "AnimatingProps", &animPropDescDefault);
+}
+
+void AnimatingPropDesc::Init(ModuleInfoBase* pModuleBase, char *param_2, char *param_3, int param_4, int param_5,
+        Tools_AnimEntry* pAnimEntry, int animEntryCount, Tools_AnimEvent *pAnimEvent, int animEventCount, bool param_10, u16 _defaultGameObjFlags) {
+    StaticPropDescriptor::Init(pModuleBase, param_2, param_3, param_4, param_5);
+    defaultGameObjFlags = _defaultGameObjFlags;
+    soundEventIndex = -1;
+    nmbrOfSoundEvents = 0;
+    pSoundEvents = NULL;
+    if (stricmp(param_2, "") != 0) {
+        mAnimScript.Init(modelName);
+        mAnimDesc.Init(pAnimEntry, animEntryCount, &mAnimScript);
+        mAnimEventDesc.Init(pAnimEvent, animEventCount, &mAnimScript);
+    } else {
+        mAnimDesc.pEntries = pAnimEntry;
+        mAnimDesc.unk4 = animEntryCount;
+        mAnimEventDesc.pEvents = pAnimEvent;
+        mAnimEventDesc.mCount = animEventCount;
+    }
+    unk10C = param_10;
+}
+
+void AnimatingPropDesc::Load(KromeIni* pIni) {
+    StaticPropDescriptor::Load(pIni);
+    KromeIniLine* pLine = pIni->GotoLine(modelName, NULL);
+    char* pString = NULL;
+    while (pLine != NULL && (pLine->section != NULL || pLine->pFieldName != NULL || pLine->comment != NULL)) {
+        if (pLine->pFieldName != NULL) {
+            if (stricmp(pLine->pFieldName, "idleSound") == 0 && pLine->AsString(0, &pString)) {
+                soundEventIndex = SoundBank_ResolveSoundEventIndex(pString);
+            } else if (stricmp(pLine->pFieldName, "soundEvents") == 0 && pLine->elementCount > 0) {
+                nmbrOfSoundEvents = pLine->elementCount;
+                pSoundEvents = (SoundEvent*)Heap_MemAlloc(nmbrOfSoundEvents * sizeof(SoundEvent));
+                for(int i = 0; i < nmbrOfSoundEvents; i++) {
+                    pSoundEvents[i].animEventName = mAnimScript.GetEventByName(Str_Printf("SoundEvent%d", i + 1));
+                    pLine->AsString(i, &pString);
+                    pSoundEvents[i].soundEventIndex = SoundBank_ResolveSoundEventIndex(pString);
+                }
+            }
+        }
+        pLine = pIni->GetLineWithLine(pLine);
+    }
+}
+
+void AnimatingProp::Init(GameObjDesc* pDesc) {
+    GameObject::Init(pDesc);
+    pModel = Model::Create(GetDesc()->mAnimScript.GetMeshName(), 
+        GetDesc()->mAnimScript.GetAnimName());
+    pModel->renderType = 3;
+    collisionInfo.Init(true, GetDesc()->collisionInfoFlags, this);
+    lodManager.Init(pModel, 0, &GetDesc()->lodDesc);
+    collide = true;
+    mAnimScript.Init(&GetDesc()->mAnimScript);
+    mAnimManager.Init(&GetDesc()->mAnimDesc);
+    mAnimEventManager.Init(&GetDesc()->mAnimEventDesc);
+    gameObjFlags.Init((CommonGameObjFlags)GetDesc()->defaultGameObjFlags);
+    soundHelper.Init();
+}
+
+bool AnimatingProp::LoadLine(KromeIniLine* pLine) {
+    return StaticProp::LoadLine(pLine) || gameObjFlags.LoadLine(pLine);
+}
+
+void AnimatingProp::LoadDone(void) {
+    StaticProp::LoadDone();
+    if (GetDesc()->unk10C) {
+        // set initial animation to idling animation
+        MKAnim* pIdleAnim = mAnimScript.GetAnim("idle");
+        if (pIdleAnim) mAnimScript.SetAnim(pIdleAnim);
+    }
+    gameObjFlags.SetDefaultFlags();
+}
+
+extern void SoundBank_Stop(int*);
+
+void AnimatingProp::Reset(void) {
+    GameObject::Reset();
+    gameObjFlags.Reset();
+    collisionInfo.bEnabled = gameObjFlags.CheckFlags(GameObjFlags_Enabled) && gameObjFlags.CheckFlags(GameObjFlags_Visible);
+    mAnimScript.Reset();
+    SoundBank_Stop(&soundHelper.unk0);
+}
+
+void AnimatingProp::Update(void) {
+    bool flagsCheck = gameObjFlags.CheckFlags(GameObjFlags_Enabled) && gameObjFlags.CheckFlags(GameObjFlags_Visible);
+    if (GetDesc()->soundEventIndex > -1) {
+        soundHelper.Update(GetDesc()->soundEventIndex, false, flagsCheck && mAnimScript.currAnim, this, NULL, -1.0f, 0);
+    }
+    if (flagsCheck) {
+        mAnimScript.Animate();
+        mAnimScript.Apply(pModel->pAnimation);
+        mAnimEventManager.Update(&mAnimScript);
+        if (GetDesc()->nmbrOfSoundEvents > 0) {
+            int i = 0;
+            char* event;
+            while (event = mAnimScript.GetEvent(i)) {
+                for(int j = 0; j < GetDesc()->nmbrOfSoundEvents; j++) {
+                    if (event == GetDesc()->pSoundEvents[j].animEventName) {
+                        SoundEventHelper soundEvent;
+                        soundEvent.unk0 = -1;
+                        soundEvent.Update(GetDesc()->pSoundEvents[j].soundEventIndex, false, true,
+                            this, NULL, -1.0f, 0);
+                        break;
+                    }
+                }
+                i++;
+            }
+        }
+    }
+}
+
+void AnimatingProp::Draw(void) {
+    if (gameObjFlags.CheckFlags(GameObjFlags_Enabled) && gameObjFlags.CheckFlags(GameObjFlags_Visible)) {
+        StaticProp::Draw();
+    }
+}
+
+extern void SoundBank_Stop(int*);
+void AnimatingProp::Message(MKMessage* pMessage) {
+    gameObjFlags.Message(pMessage);
+    collisionInfo.bEnabled = gameObjFlags.CheckFlags(GameObjFlags_Enabled) &&
+        gameObjFlags.CheckFlags(GameObjFlags_Visible);
+    switch (pMessage->unk0) {
+        case 10:
+            Activate(true);
+            break;
+        case 11:
+            Activate(false);
+            break;
+        case 12:
+            Enable(true);
+            break;
+        case 13:
+            Enable(false);
+            break;
+        case 14:
+            Show(true);
+            break;
+        case 15:
+            Show(false);
+            break;
+    }
+    GameObject::Message(pMessage);
+    if (pMessage->unk0 == -4 || !collisionInfo.bEnabled) {
+        SoundBank_Stop(&soundHelper.unk0);
+    }
+}
+
+void AnimatingProp::Show(bool bShow) {
+    if (bShow) {
+        gameObjFlags.Set(GameObjFlags_Visible);
+    } else {
+        gameObjFlags.Clear(GameObjFlags_Visible);
+    }
+}
+
+void AnimatingProp::Activate(bool bActivate) {
+    if (bActivate) {
+        gameObjFlags.Set(GameObjFlags_Active);
+    } else {
+        gameObjFlags.Clear(GameObjFlags_Active);
+    }
+}
+
+void AnimatingProp::Enable(bool bEnable) {
+    if (bEnable) {
+        gameObjFlags.Set(GameObjFlags_Enabled);
+    } else {
+        gameObjFlags.Clear(GameObjFlags_Enabled);
+    }
+}
