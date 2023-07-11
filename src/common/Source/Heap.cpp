3b8a0bb9a8964714028b9fd6047261bf48aaf91b
1superchip <1superchip@gmail.com>
2023-07-10 22:13:25 -0500

    Add function documentation to Heap.cpp

--- src/common/Source/Heap.cpp
+++ src/common/Source/Heap.cpp
@@ -17,15 +17,23 @@ void Heap_DeinitModule(void) {
 	heapInitialised = false;
 }
 
+/// @brief Returns the current amount of used memory
+/// @param  None
+/// @return Current amount of used memory
 int Heap_MemoryUsed(void) {
     return OSGetConsoleSimulatedMemSize() - OSCheckHeap(0);
 }
 
-// alignment is unused!
+/// @brief Allocates a block of memory, does not use alignment
+/// @param size size of block to allocate
+/// @param alignment Unused parameter for alignment
+/// @return Allocated block of memory
 void* Heap_MemAllocAligned(int size, int alignment) {
     return OSAllocFromHeap(__OSCurrHeap, size);
 }
 
+/// @brief Frees an aligned memory block
+/// @param ptr Memory to free
 void Heap_MemFreeAligned(void* ptr) {
     OSFreeToHeap(__OSCurrHeap, ptr);
 }
@@ -45,6 +53,10 @@ void Heap_MemFree(void* ptr) {
     OSFreeToHeap(__OSCurrHeap, ptr);
 }
 
+/// @brief Checks the heap
+/// @param file Filename to print
+/// @param lineNumber Line number to print
+/// @return Returns 0 if no error occurs otherwise exits the program
 int Heap_Check(char* file, int lineNumber) {
     if ((int)OSCheckHeap(0) == -1) {
         printf("\nCorrupt HEAP\n");
