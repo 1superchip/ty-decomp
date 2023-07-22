6c102a75ce56c52fc36bb2d7e9c7ff2de5856a3f
1superchip <1superchip@gmail.com>
2023-07-22 11:42:41 -0500

    Link __start.c
    Hopefully this gets the decomp shiftable

---
+++ src/common/gc/MKShadow_GC_rodata.s
@@ -0,0 +1,17 @@
+.include "macros.inc"
+
+.section .rodata
+
+.global lbl_8023ffd0
+lbl_8023ffd0:
+/* 8023FFD0 00000000 */ .4byte      0x00000000
+/* 8023FFD4 00000000 */ .4byte      0x00000000
+/* 8023FFD8 00000000 */ .4byte      0x00000000
+/* 8023FFDC 00000000 */ .4byte      0x00000000
+
+.global lbl_8023ffe0
+lbl_8023ffe0:
+/* 8023FFE0 00000000 */ .4byte      0x00000000
+/* 8023FFE4 00000000 */ .4byte      0x00000000
+/* 8023FFE8 00000000 */ .4byte      0x00000000
+/* 8023FFEC 00000000 */ .4byte      0x00000000
